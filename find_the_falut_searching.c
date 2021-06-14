1dd6ac6517a2bd6f94a2c56a28cb0a38fb1893d4
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-06-15 00:29:35 +0530

    Problem No. 121
    Title -> Find the fault
    
    
    
    Statement: in an apartment of n floors, peoples generally uses a lift to reach on their desired floor.
    
    One day a fault has occurred in lift. The fault is happened on a single floor such that till that floor lift is working well but for that
    
    faulty floor and for all the above floors lift is not moving.
    
    Due to the time shortage the lift repairman wants to find the faulty floor in minimum time.
    
    So your task is to help that repairman by creating a program such that your program can find the faulty floor in log n time.
    
    We are providing you n length array that contains 1’s and 0's.
    
    In that array 1 is denoting the lift is working fine and 0 denoting lift is not moving for that particular floor.
    
    Treat the index 0 as ground floor and n-1 index as top floor.
    
    For example, suppose array lift={1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0}
    
    Here first 0 is available on i=11 index so that means the faulty floor is 11th floor.
    
    So output should contains the 11 denoting faulty floor location.
    
    Input: first line contains an integer n denoting size of array.
    
    Second line contains n integers as array elements denoting lift is working fine or not on particular floor by using 1 and 0.
    
    Output: output contains an integer denoting faulty floor.
    
    Constraint: 10<=n<=10000
    
    Example:
    
    16
    
    1 1 1 1 1 1 1 1 1 1 1 0 0 0 0 0
    
    11

---
+++ find_the_falut_searching.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    int i, n;
+    scanf("%d",&n);
+
+    int arr[n];
+
+    for(i = 0; i < n; i++)
+        scanf("%d",&arr[i]);
+
+    for(i = n - 1; i >= 0; i--)
+    {
+        if(arr[i] != 0)
+        {
+            break;
+        }
+    }
+
+    printf("%d", ++i);
+    return 0;
+}
