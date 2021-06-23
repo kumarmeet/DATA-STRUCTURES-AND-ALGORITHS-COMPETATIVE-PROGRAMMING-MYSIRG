b64fc8483c2e72b7ab635dd3f26aef55011de34b
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-06-23 22:45:22 +0530

    Problem No.164
    Title -> Create binary search tree
    
    Statement: create a binary search tree, from given array.
    
    treat the first element of array as root node of tree and then insert all elements of array in binary search tree.
    
    suppose array elements are 50 30 60 22 38 55 34 so final BST should look like
    
    once you submit the code internally program will take the inorder traversal of your tree and print it.
    
    input: first line contains an integer n denoting number of elements of array.
    
    second line contains n space separated integers denoting elements of array.
    
    output: contains inorder traversal of BST.
    
    constraints: 5<=n<=20
    
    Example:
    
    7
    
    50 30 60 22 38 55 34
    
    22 30 34 38 50 55 60

---
+++ create_BST.c
@@ -0,0 +1,66 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int info;
+    struct node *lchild;
+    struct node *rchild;
+}*root;
+
+struct node * createNode(struct node*, int);
+void insertNode(struct node**, int);
+void inorder(struct node *);
+
+struct node *root = NULL;
+
+int main()
+{
+    int arr[] = {50,30,60,22,38,55,34};
+    int n = 7;
+
+    createBST(arr, n);
+
+    inorder(root);
+    return 0;
+}
+
+void createBST(int *arr,int n)
+{
+    int i;
+
+    for(i = 0; i < n; i++)
+        insertNode(&root, arr[i]);
+}
+
+void inorder(struct node *t)
+{
+    if(t)
+    {
+        inorder(t->lchild);
+        printf("%d ",t->info);
+        inorder(t->rchild);
+    }
+}
+
+void insertNode(struct node **b, int item)
+{
+    struct node *n;
+
+    if(!(*b))
+        (*b) = createNode(&b, item);
+
+    if(item < (*b)->info)
+        insertNode(&(*b)->lchild, item);
+    else if(item > (*b)->info)
+        insertNode(&(*b)->rchild, item);
+}
+
+struct node * createNode(struct node *b, int item)
+{
+    struct node *n;
+    n = (struct node*)malloc(sizeof(struct node));
+    n->lchild = n->rchild = NULL;
+    n->info = item;
+    return n;
+}
