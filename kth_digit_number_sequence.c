92946c9c78fedb362ade9f54a8899090c8d1fdd7
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2021-07-15 11:30:38 +0530

    Problem 142
    Title -> Find the Kth digit in the number sequence
    
    
    
    Statement: write a program to accept two integers n and m such that n is always greater than m.
    
    Treat n and m as two end points of limit or range (including n and m).
    
    Now your task is to find the kth digit found in the sequence when we print the range from m to n in reverse.
    
    For example, suppose m=25 and n=30 then m to n forward sequence look like
    
    252627282930
    
    But reverse of the above sequence look like
    
    039282726252
    
    And if suppose we have to find k=6th digit of reverse sequence then 6th digit of the reverse sequence is 2which is shown by the
    
    red color in sequence. (count the position from left to right in reverse sequence)
    
    Input: first line contains two integers m and n respectively where m<n denoting end points of range.
    
    Second line contain an integer denoting position of digit in reverse sequence.
    
    Output : contains an integer d denoting kth digit present in reverse sequence.
    
    Constraints:1<=n<=100
    
    1<=m<=100
    
    n>m
    
    Example:
    
    25 30
    
    6
    
    2

---
+++ kth_digit_number_sequence.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int val1, val2;
+
+    scanf("%d%d", &val1, &val2);
+  
+    if(val1 > val2)
+       exit(0);
+
+    size_t size = (val2 - val1) + 1;
+
+    int arr[size];
+    int j = 0;
+
+    for(int i = val1; i <= val2; i++)
+    {
+        arr[j] = i;
+        j++;
+    }
+
+    int extract[20] = {0};
+    int temp, r;
+    j = 0;
+    int m = 0;
+
+    for(int i = 0; i < size; i++) // take element from arr[] and split into single digit
+    {
+        temp = arr[i];
+        while(temp)
+        {
+            r = temp % 10;
+            extract[j] = r; //copy single digit from arr[]
+            temp = temp / 10;
+            j++;
+        }
+
+        swap(&extract[m], &extract[m++]); // swap all element and rearrange increasing order
+        m++;
+    }
+
+    int swaped[j];
+
+    for(int i = 0; i < j; i++)
+      swaped[i] = extract[i];
+
+    int k;
+
+    scanf("%d", &k);
+
+    printf("%d", swaped[k]);
+
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
